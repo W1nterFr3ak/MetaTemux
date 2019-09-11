0251dd1baa0f9d1879774fcfc6c771181f434915
W1nterFr3ak <WinterFreak@protonmail.com>
2019-09-11 15:25:07 -0400

    cleanin

--- metatermux.py
+++ metatermux.py
@@ -4,35 +4,20 @@ import sys
 
 def verluchie():
 	try:
+		
 		os.system('echo "\\e[4;34mStarting verluchie Script----------------------->\\e[0m"')
 		os.system('apt install autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev \
 					libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev \
-					libxslt-dev ncurses-dev pkg-config postgresql-contrib wget make ruby-dev libgrpc-dev ncurses-utils termux-tools -y')
+					libxslt-dev ncurses-dev pkg-config postgresql-contrib wget make ruby-dev libgrpc-dev ncurses-utils termux-tools gem -y')
 		os.system('echo "####################################"')
 		os.system('echo "Downloading & Extracting....."')
 		
-		os.system('cd $HOME')
-		os.system('curl -LO https://github.com/rapid7/metasploit-framework/archive/4.16.4.tar.gz')
-		os.system('tar -xf $HOME/4.16.4.tar.gz')
-		os.system('mv $HOME/metasploit-framework-4.16.4 $HOME/metasploit-framework')
-		os.system('cd $HOME/metasploit-framework')
-		os.system("sed '/rbnacl/d' -i Gemfile.lock")
-		os.system("sed '/rbnacl/d' -i metasploit-framework.gemspec")
-		os.system("gem install bundler")
-		os.system("bundle config build.nokogiri --use-system-libraries")
-		os.system('gem install nokogiri -- --use-system-libraries')
-		os.system("sed 's|grpc (.*|grpc (1.4.1)|g' -i $HOME/metasploit-framework/Gemfile.lock")
-		os.system("gem unpack grpc -v 1.4.1")
-		os.system("cd grpc-1.4.1")
-		os.system("curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec")
-		os.system("curl -L https://wiki.termux.com/images/b/bf/Grpc_extconf.patch -o extconf.patch")
-		os.system("patch -p1 < extconf.patch")
-		os.system("gem build grpc.gemspec")
-		os.system("gem install grpc-1.4.1.gem")
-		os.system("cd ..")
-		os.system("rm -r grpc-1.4.1")
-		os.system("cd $HOME/metasploit-framework")
-		os.system("bundle install -j5")
+		os.system("cd $HOME && curl -LO https://github.com/rapid7/metasploit-framework/archive/4.16.4.tar.gz && tar -xf $HOME/4.16.4.tar.gz && \
+					mv $HOME/metasploit-framework-4.16.4 $HOME/metasploit-framework && cd $HOME/metasploit-framework && sed '/rbnacl/d' -i Gemfile.lock && \
+					sed '/rbnacl/d' -i metasploit-framework.gemspec && gem install bundler && bundle config build.nokogiri --use-system-libraries && \
+					gem install nokogiri -- --use-system-libraries && sed 's|grpc (.*|grpc (1.4.1)|g' -i $HOME/metasploit-framework/Gemfile.lock  && gem unpack grpc -v 1.4.1 && cd grpc-1.4.1 && \
+					curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec && curl -L https://wiki.termux.com/images/b/bf/Grpc_extconf.patch -o extconf.patch && \
+					patch -p1 < extconf.patch && gem build grpc.gemspec && gem install grpc-1.4.1.gem && cd .. && rm -r grpc-1.4.1 && cd $HOME/metasploit-framework && bundle install -j5")
 		
 		os.system("$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;")
 		os.system("rm ./modules/auxiliary/gather/http_pdf_authors.rb")
